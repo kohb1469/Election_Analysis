2835ac010a816de1338de2f42825b9065f87773a
Bryan Koh <bkoh@usc.edu>
2022-07-05 23:54:20 -0700

    Adding Election audit

--- PyPoll.py
+++ PyPoll.py
@@ -2,29 +2,63 @@
 import csv
 import os
 
+
 # Create a filename variable to a direct path to the file
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = " "
+winning_count = 0
+winning_percentage = 0 
 
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
-    print(headers)
+    
+    #counting total votes
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+   
+    print(candidate_votes)
 
-
-
-# Using the open() function with the "w" mode we will write data to the file.
-with open(file_to_save, "w") as txt_file:
-#write something into file
-
-    txt_file.write("Arapahoe, ")
-    txt_file.write("Denver, ")
-    txt_file.write("Jefferson")
 #1. Total number of votes cast
-#2. A complete list of condidates who received votes
-#3. The percentage of votes each candidate won
-#4. The total number of votes each candidate won 
-#5. The winner of the election based on popular vote.
+    print(total_votes)
 
-#close file
-#election_data.close()
+#2. A complete list of condidates who received votes
+    
+#3. The percentage of votes each candidate won
+    for candidate, votes in candidate_votes.items():
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate}: received {(vote_percentage):.1f}% of the vote.")
+        
+        #4. The total number of votes each candidate won
+        print(f"{candidate}: received {votes} total votes")
+
+        #5. The winner of the election based on popular vote.
+        if winning_count < votes and (winning_percentage < vote_percentage):
+            winning_count = votes
+            winning_candidate = candidate
+            winning_percentage = vote_percentage
+    
+    
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    
+
+
+
+
+
+
